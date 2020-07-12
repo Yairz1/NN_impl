0fac6dcc025e5e1ea57acb6fb96cc6eadcc791d6
yair_zohav <yairzohav@gmail.com>
2020-07-12 18:01:26 +0300

    upload project

--- loss_function.py
+++ loss_function.py
@@ -16,11 +16,4 @@ def objective_soft_max(X, W, C):
 
 
 def objective_soft_max_gradient(X, W, C):
-    l = C.shape[1]
-    m = X.shape[1]
-    objective_value = 0
-    weighted_sum = sum(exp(X.T @ W), axis=1)
-    wighted_sum_matrix = diag(1 / weighted_sum)
-    for k in range(l):
-        objective_value = objective_value + C[:, k].T @ log(wighted_sum_matrix @ exp(X.T @ W[:, k]))
-    return - objective_value / m
+    pass
