2ac1d3123cd884a9931bef85d74ca033dcedd018
yair_zohav <yairzohav@gmail.com>
2020-07-13 16:28:27 +0300

    upload project

--- Tests/test_loss.py
+++ Tests/test_loss.py
@@ -6,6 +6,18 @@ def f(X, W):
     return X.T @ W
 
 
+def objective_soft_max_old(X, W, C):
+    l = C.shape[1]
+    m = X.shape[1]
+    objective_value = 0
+    eta = find_eta(X, W)
+    XT_W = X.T @ W - eta
+    weighted_sum = sum(exp(XT_W), axis=1)  # todo minus eta ?
+    wighted_sum_matrix = diag(1 / weighted_sum)
+    for k in range(l):
+        objective_value = objective_value + C[:, k].T @ log(wighted_sum_matrix @ exp(XT_W[:, k]))
+    return - objective_value / m
+
 class LossTests(unittest.TestCase):
 
     def test00_objective_soft_max_sanity_test(self):
@@ -21,4 +33,13 @@ class LossTests(unittest.TestCase):
         except:
             self.assertTrue(False)
 
+    def test01_objective_soft_max_sanity_test(self):
+        X = np.random.randn(5, 3)
+        C = array([[1, 0],
+                   [0, 1],
+                   [1, 0]])
+        W = np.random.randn(5, 2)
+        c1 = np.round(objective_soft_max(X, W, C), 10)
+        c2 = np.round(objective_soft_max_old(X, W, C), 10)
 
+        self.assertEqual(c1, c2)
--- loss_function.py
+++ loss_function.py
@@ -1,19 +1,28 @@
 import numpy as np
-from numpy import exp, zeros, diag, log, array, sum
+from numpy import exp, zeros, diag, log, array, sum, max, divide
 import scipy
 from activation import softmax
+
+
+def find_eta(X, W):
+    return max(X.T @ W, axis=0)
 
 
 def objective_soft_max(X, W, C):
     l = C.shape[1]
     m = X.shape[1]
     objective_value = 0
-    weighted_sum = sum(exp(X.T @ W), axis=1)  # todo minus eta ?
-    wighted_sum_matrix = diag(1 / weighted_sum)
+    eta = find_eta(X, W)
+    XT_W = X.T @ W - eta
+    weighted_sum = sum(exp(XT_W), axis=1)  # todo minus eta ?
     for k in range(l):
-        objective_value = objective_value + C[:, k].T @ log(wighted_sum_matrix @ exp(X.T @ W[:, k]))
+        objective_value = objective_value + C[:, k].T @ log(divide(exp(XT_W[:, k]), weighted_sum))
     return - objective_value / m
 
 
-def objective_soft_max_gradient(X, W, C):
+def objective_soft_max_gradient_X(X, W, C):
     pass
+
+def objective_soft_max_gradient_W(X, W, C):
+    pass
+
