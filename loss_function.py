5db720d0634315bf4e4886d9fdbc12938402b9b8
yair_zohav <yairzohav@gmail.com>
2020-07-28 16:38:48 +0300

    added neuralnetwork and forward impl

--- loss_function.py
+++ loss_function.py
@@ -30,12 +30,13 @@ def objective_soft_max(X, W, C, WT_X=None):
     return - objective_value / m
 
 
+# todo if i sent X.T W should do transpose
 def objective_soft_max_gradient_X(X, W, C):
     WT_X = W.T @ X
     m = X.shape[1]
     return (1 / m) * W @ (divide(exp(WT_X), sum(exp(WT_X), axis=0)) - C)
 
-
+# todo if i sent X.T W should do transpose
 def objective_soft_max_gradient_W(X, W, C):
     m = C.shape[1]
     eta = find_eta(X, W)
